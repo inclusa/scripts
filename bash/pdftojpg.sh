6ae742e74fd004dc45af51bba38d405c52bcdea3
Alfons Rovira <inclusa@gmail.com>
2018-09-03 21:04:10 +0200

    Remove the color

--- bash/pdftojpg.sh
+++ bash/pdftojpg.sh
@@ -1,7 +1,7 @@
 # Goal: Convert file PDF to JPG
 #
 echo " "
-echo "Amb aquest script podràs convertir un arxiu \e[1;96mPDF\e[0m:en un gràfic \e[1;96mJPG\e[0m:"
+echo "Amb aquest script podràs convertir un arxiu PDF en un gràfic JPG."
 echo " "
 echo "Introdueix l'arxiu què vols generar: "
 echo " "
@@ -10,6 +10,6 @@ read PDF
 echo "--------------------------"
 echo " "
 gm convert -density 150 -resize 600 $PDF +adjoin %02d.jpg
-echo "Recorda que el nou arxiu s'anomenarà: \e[1;33m00.jpg\e[0m:"
+echo "Recorda que el nou arxiu s'anomenarà: 00.jpg"
 echo " "
 echo " "
